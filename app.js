e2357f2c196146e1d6006229e72c16740c408121
Adriano Lisboa <adrianolic@gmail.com>
2020-09-14 14:54:31 -0300

    Remove unused view engine plugin and configuration

--- app.js
+++ app.js
@@ -3,7 +3,6 @@
 const createError = require("http-errors");
 const express = require("express");
 const helmet = require("helmet");
-const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 
@@ -12,14 +11,10 @@ const indexRouter = require("./routes/index");
 const app = express();
 app.use(helmet());
 
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "pug");
-
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/healthcheck", require("./routes/healthcheck.routes"));
