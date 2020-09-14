4f61c17fc953140c5bf773bda6d1a75742d95c97
Adriano Lisboa <adrianolic@gmail.com>
2020-09-14 13:22:44 -0300

    Add helmet library

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const createError = require("http-errors");
 const express = require("express");
+const helmet = require("helmet");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
@@ -7,8 +8,8 @@ const logger = require("morgan");
 const indexRouter = require("./routes/index");
 
 const app = express();
+app.use(helmet());
 
-// view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
@@ -21,18 +22,14 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 app.use("/healthcheck", require("./routes/healthcheck.routes"));
 
-// catch 404 and forward to error handler
 app.use((req, res, next) => {
   next(createError(404));
 });
 
-// error handler
 app.use((err, req, res) => {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
   res.render("error");
 });
