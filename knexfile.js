2c2812735ca88d99bee01d916e39d41261c32f34
Adriano Lisboa <adrianolic@gmail.com>
2020-09-16 14:26:13 -0300

    Fix docker configuration

--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,7 @@
 module.exports = {
   test: {
-    client: 'pg',
-    connection: 'postgres://localhost/sample_api_test',
+    client: "pg",
+    connection: "postgres://localhost/sample_api_test",
     migrations: {
       directory: `${__dirname}/db/migrations`,
     },
@@ -10,8 +10,8 @@ module.exports = {
     },
   },
   development: {
-    client: 'pg',
-    connection: 'postgres://localhost/sample_api_dev',
+    client: "pg",
+    connection: process.env.DATABASE_URL,
     migrations: {
       directory: `${__dirname}/db/migrations`,
     },
@@ -20,7 +20,7 @@ module.exports = {
     },
   },
   production: {
-    client: 'pg',
+    client: "pg",
     connection: process.env.DATABASE_URL,
     migrations: {
       directory: `${__dirname}/db/migrations`,
